d25995dc1d6a578799fe16e4e2ed098740869f94
Sneha Senthilkumar <ssneha@outlook.com>
2019-12-11 16:37:26 -0500

    signup working YEET

---
+++ app/signup.js
@@ -0,0 +1,30 @@
+$(function() {
+  console.log("hi"); 
+  $('#start').click('submit', createAccount);
+});
+
+const createAccount = async function(event) {
+  console.log("hello")
+  event.preventDefault();
+    let response = await axios ({
+      method: 'post',
+      url: 'http://localhost:3000/account/create',
+      data: {
+        "name": $(`#username`).val(),
+        "pass": $(`#password`).val(),
+            "data": {        
+            "email": $(`#email`).val(),
+            "first": $(`#first`).val(),
+            "last": $(`#last`).val(),
+            "phone": $(`#phone`).val(),
+            "sign": $(`#sign`).val(),
+            "city": $(`#city`).val(),
+            "message": $(`#message`).val(),
+            }
+        }
+    });
+    console.log("done!");
+    window.location.href = "http://localhost:3001/"
+  
+}
+
