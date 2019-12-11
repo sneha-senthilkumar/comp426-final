c5c83180904a6ed66226c22cdc2bc32d40df4d30
sydechen <sydechen@gmail.com>
2019-12-11 17:42:55 -0500

    pics

--- app/profile.js
+++ app/profile.js
@@ -1,24 +1,21 @@
-const createAccount = async function(event) {
-    console.log("hello")
-    event.preventDefault();
-      let response = await axios ({
-        method: 'get',
-        url: 'http://localhost:3000/account/status',
-        data: {
-          "name": $(`#username`).val(),
-          "pass": $(`#password`).val(),
-              "data": {        
-              "email": $(`#email`).val(),
-              "first": $(`#first`).val(),
-              "last": $(`#last`).val(),
-              "phone": $(`#phone`).val(),
-              "sign": $(`#sign`).val(),
-              "city": $(`#city`).val(),
-              "message": $(`#message`).val(),
-              }
-          }
-      });
-      console.log("done!");
-      window.location.href = "http://localhost:3001/"
-    
-  }
+export const renderProfile = function(user) {
+    const getProfile = async function(event) {
+        console.log("hello")
+        var jwt = localStorage.getItem('jwt');
+        let response = await axios ({
+            method: 'get',
+            url: 'http://localhost:3000/account/status',
+            headers: {'Bearer': jwt},
+          });
+          return response;
+        }
+    let username = response.data.name;
+    let name = `${response.data.data.first} ${response.data.data.last}`;
+    let email = data.data.email;
+    let phone = data.data.phone;
+    let sign = data.data.sign;
+    let city = data.data.city;
+    let message = data.data.message;
+      
+}
+
