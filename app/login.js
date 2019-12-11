ec54aac6990b3afb95a46d4e3009d6f322e68fad
sydechen <sydechen@gmail.com>
2019-12-11 11:57:07 -0500

    login script copied and linked

---
+++ app/login.js
@@ -0,0 +1,28 @@
+$(function() {
+  const $form = $('#login-form');
+  const $message = $('#message');
+
+  $form.submit(function(e) {
+    e.preventDefault();
+
+    $message.html('');
+
+    const data = $form.serializeArray().reduce((o, x) => {
+      o[x.name] = x.value;
+      return o;
+    }, {});
+    
+    $.ajax({
+      url: 'https://localhost:3000/public',
+      type: 'POST',
+      data,
+      xhrFields: {
+          withCredentials: true,
+      },
+    }).then(() => {
+      $message.html('<span class="has-text-success">Success! You are now logged in.</span>');
+    }).catch(() => {
+      $message.html('<span class="has-text-danger">Something went wrong and you were not logged in. Check your email and password and your internet connection.</span>');
+    });
+  });
+});
