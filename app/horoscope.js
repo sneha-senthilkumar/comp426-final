968be203b78b7c89b91a226ea5d9f7f74a6f029a
Sneha Senthilkumar <ssneha@outlook.com>
2019-12-12 02:49:25 -0500

    s

--- app/horoscope.js
+++ app/horoscope.js
@@ -5,7 +5,9 @@ const profile = {
 }
 
 export const renderHoroscope = async function(){
-    let sign = "";
+    var today = new Date();
+    var date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
+    $('#date').replaceWith(`<h3 id="date">${date}</h3>`)
     axios.get('http://localhost:3000/account/status',{
         headers: {'Authorization': 'Bearer ' + jwt}
     })
@@ -20,28 +22,30 @@ export const renderHoroscope = async function(){
             let city = response.user.data.city;
             let message = response.user.data.message;
             
+            let head = `<a id="welcome" class="navbar-brand" href="#" style="color: rgb(255,253,253);"><i class="fa fa-user-circle-o"></i>&nbsp; Welcome ${response.first}!</a>`;
+            $('#welcome').replaceWith(head);
 
         })
         .catch(function (error) {
             console.log(error);
         });
 
-        let horo = await axios ({
-            method: 'get',
-            url: 'http://ohmanda.com/api/horoscope/' + sign.toLowerCase()
-        });
+        // let horo = await axios ({
+        //     method: 'get',
+        //     url: 'http://ohmanda.com/api/horoscope/' + sign.toLowerCase()
+        // });
        // let horo = axios.get('http://ohmanda.com/api/horoscope/' + sign.toLowerCase(),{
        //     headers: {'Access-Control-Allow-Origin': '*'}
        // });
 
-       console.log(horo);
+       //console.log(horo);
        // let app = `<div class="content"><h1>${name}<i style="font-size: 30px;"> @${username}</i></h1><div class="container" style="text-align: right;"><button id="editB">Edit</button><div class="info">
        // <img id="propic" src=${picurl} alt="propic goes here">
        // <p>${sign} | ${city}</p>
        // <p>${email} | ${phone}<p>
        // <p class="about">${message}</p></div></div></div>`;
 
-       $('#daily').replaceWith(horo);
+       //$('#daily').replaceWith(horo);
 
 
        // $(document).on("click", "#editB", {res:response},handleEditButtonPress);
