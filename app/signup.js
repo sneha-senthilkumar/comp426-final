b29c982b17860205ff7520e05c3b2a97f6593a54
sydechen <sydechen@gmail.com>
2019-12-12 09:03:34 -0500

    trying to post to public

--- app/signup.js
+++ app/signup.js
@@ -4,7 +4,9 @@ $(function() {
 });
 
 const createAccount = async function(event) {
-  console.log("hello")
+  console.log("hello");
+  let username = $(`#username`).val()
+
   event.preventDefault();
     let response = await axios ({
       method: 'post',
@@ -23,24 +25,25 @@ const createAccount = async function(event) {
             }
         }
     });
-    
-    let accountupdate = await axios ({
-      method: 'post',
-      url: 'http://localhost:3000/public/users',
-      data: {
-        "data": {
-          "username": $(`#username`).val(),
-          "email": $(`#email`).val(),
-          "first": $(`#first`).val(),
-          "last": $(`#last`).val(),
-          "phone": $(`#phone`).val(),
-          "sign": $(`#sign`).val(),
-          "city": $(`#city`).val(),
-          "message": $(`#message`).val(),
-          "type": "merge"
-      }
-  },
-  });
+    // let upurl =`https://localhost:3000/public/users/`
+
+  //   let accountupdate = await axios ({
+  //     method: 'post',
+  //     url: upurl,
+  //     data: {
+  //       "data" : {
+  //         "username": $(`#username`).val(),
+  //         "email": $(`#email`).val(),
+  //         "first": $(`#first`).val(),
+  //         "last": $(`#last`).val(),
+  //         "phone": $(`#phone`).val(),
+  //         "sign": $(`#sign`).val(),
+  //         "city": $(`#city`).val(),
+  //         "message": $(`#message`).val(),
+  //       },
+  //       "type":"merge"
+  //     },
+  // });
     console.log("done!");
     window.location.href = "index.html";
   
